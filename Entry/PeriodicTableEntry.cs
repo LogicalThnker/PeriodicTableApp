70902e18210386a1a2226d9908138d6e4800ebe7
LogicalThnker <9027884+LogicalThnker@users.noreply.github.com>
2026-09-13 14:22:46 -0400

    Removed a bunch of unused "Using..." at top of file. Added a "Using PeriodicTableConsoleApp.ClassLib;" to the top of "PeriodicTableEntry.cs" and 2 lines just creating and using a 'hydrogen' example.

--- Entry/PeriodicTableEntry.cs
+++ Entry/PeriodicTableEntry.cs
@@ -1,10 +1,13 @@
-﻿namespace PeriodicTableConsoleApp.Entry
+﻿using PeriodicTableConsoleApp.ClassLib;
+
+namespace PeriodicTableConsoleApp.Entry
 {
     public class PeriodicTableEntry
     {
         static void Main(string[] args)
         {
-            
+            Element hydrogen = PeriodicTable.Get("H");
+            Console.WriteLine(hydrogen.Name);
         }
     }
 }
