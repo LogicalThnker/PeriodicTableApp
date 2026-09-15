d312420597b55d7ffe7e07ef855d2f17b3c1aac3
LogicalThnker <9027884+LogicalThnker@users.noreply.github.com>
2026-09-14 20:10:04 -0400

    Removed all old Isotope.cs data types/etc. Not going to be using Isotope for a very long time, and the old naming/etc was not correct anyways.

--- ClassLib/Isotope.cs
+++ ClassLib/Isotope.cs
@@ -2,14 +2,6 @@
 {
     public class Isotope
     {
-        public string? Name { get; set; }
-        public int? MassNumber { get; init; }
-        public int? NeutronCount { get; init; }
-        public double? IsotopicMass { get; init; }
-        public double? NaturalAbundance { get; init; }
-        public bool IsStable { get; init; }
-        public double? HalfLifeSeconds { get; init; }
-        public double? BoilingPoint {  get; init; }
-        public double? LambdaPoint { get; init; }
+        
     }
 }
